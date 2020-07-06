e4636b2413483c4ee690ab53f087a2e8c76f273f
Fredrik Arvidsson <fredrik@fkan.se>
2020-07-06 18:42:18 +0200

    make sure fragmented responses works

--- tests/Port.Server.IntegrationTests/Given_a_port_forwarding_endpoint.When_requesting_to_port_forward.cs
+++ tests/Port.Server.IntegrationTests/Given_a_port_forwarding_endpoint.When_requesting_to_port_forward.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Buffers;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Json;
@@ -43,50 +44,47 @@ namespace Port.Server.IntegrationTests
                 _fixture.PortForwardingSocket.On<byte[]>(
                     bytes => { _fixture.PortForwardResponseReceived(bytes); });
 
-                _fixture.KubernetesApiServer.Pod.PortForward
-                    .OnConnected(
-                        new PortForward("test", "service1", 2001), async (
+                _fixture.KubernetesApiServer.Pod.PortForward.OnConnected(
+                    new PortForward("test", "service1", 2001), async (
                             socket,
                             cancellationToken) =>
-                        {
-                            await socket.HandleClosing(
-                                    cancellationToken,
-                                    async () =>
+                        await socket.HandleClosing(
+                                cancellationToken,
+                                async () =>
+                                {
+                                    await socket.SendPortAsync(
+                                            9999, cancellationToken)
+                                        .ConfigureAwait(false);
+
+                                    ValueWebSocketReceiveResult readResult;
+                                    do
                                     {
-                                        await socket.SendPortAsync(
-                                                9999, cancellationToken)
+                                        readResult = await _fixture
+                                            .ReceiveAsync(
+                                                socket,
+                                                cancellationToken)
                                             .ConfigureAwait(false);
 
-                                        ValueWebSocketReceiveResult readResult;
-                                        do
+                                        if (readResult.MessageType ==
+                                            WebSocketMessageType.Close)
                                         {
-                                            readResult = await _fixture
-                                                .ReceiveAsync(
-                                                    socket,
-                                                    cancellationToken)
-                                                .ConfigureAwait(false);
+                                            return;
+                                        }
 
-                                            if (readResult.MessageType ==
-                                                WebSocketMessageType.Close)
-                                            {
-                                                return;
-                                            }
-
-                                            if (await _fixture
-                                                .TrySendResponseAsync(
-                                                    socket, cancellationToken)
-                                                .ConfigureAwait(false))
-                                            {
-                                                return;
-                                            }
-                                        } while (readResult.EndOfMessage ==
-                                                 false ||
-                                                 cancellationToken
-                                                     .IsCancellationRequested ==
-                                                 false);
-                                    })
-                                .ConfigureAwait(false);
-                        });
+                                        if (await _fixture
+                                            .TrySendResponseAsync(
+                                                socket, cancellationToken)
+                                            .ConfigureAwait(false))
+                                        {
+                                            return;
+                                        }
+                                    } while (readResult.EndOfMessage ==
+                                             false ||
+                                             cancellationToken
+                                                 .IsCancellationRequested ==
+                                             false);
+                                })
+                            .ConfigureAwait(false));
             }
 
             protected override async Task WhenAsync(
@@ -128,7 +126,7 @@ namespace Port.Server.IntegrationTests
                     .HaveCount(_fixture.Request.Length + 1);
                 _fixture.WebSocketMessageReceived[0]
                     .Should()
-                    .Be((byte) ChannelIndex.StdIn);
+                    .Be((byte)ChannelIndex.StdIn);
                 Encoding.ASCII.GetString(
                         _fixture.WebSocketMessageReceived.GetRange(
                                 1, _fixture.Request.Length)
@@ -149,7 +147,9 @@ namespace Port.Server.IntegrationTests
             public void TestReceiveResponse()
             {
                 _fixture.PortForwardResponse.Should()
-                    .Be(_fixture.Response);
+                    .Be(_fixture.FragmentedResponses.Aggregate("", (
+                        total,
+                        response) => total + response));
             }
 
             internal sealed class Fixture : IAsyncDisposable
@@ -176,7 +176,8 @@ namespace Port.Server.IntegrationTests
                 }
 
                 internal Kubernetes.Test.API.Server.TestFramework
-                    KubernetesApiServer { get; }
+                    KubernetesApiServer
+                { get; }
 
                 internal string Request => @"
 POST /cgi-bin/process.cgi HTTP/1.1
@@ -191,7 +192,9 @@ Connection: Keep-Alive
 <?xml version=""1.0"" encoding=""utf-8""?>
 <string xmlns = ""http://clearforest.com/"">string</string>";
 
-                internal string Response => @"
+                internal string[] FragmentedResponses => new[]
+                {
+                    @"
 HTTP/1.1 400 Bad Request
 Date: Sun, 18 Oct 2012 10:36:20 GMT
 Server: Apache/2.2.14 (Win32)
@@ -199,7 +202,8 @@ Content-Length: 327
 Content-Type: text/html; charset=iso-8859-1
 Connection: Closed
 
-<!DOCTYPE HTML PUBLIC ""-//IETF//DTD HTML 2.0//EN"">
+<!DOCTYPE HTML PUBLIC ""-//IETF//DTD HTML 2.0//EN"">",
+                    @"
 <html>
 <head>
    <title>400 Bad Request</title>
@@ -209,7 +213,8 @@ Connection: Closed
    <p>Your browser sent a request that this server could not understand.</p>
    <p>The request line contained invalid characters following the protocol string.</p>
 </body>
-</html>";
+</html>"
+                };
 
                 private readonly IMemoryOwner<byte> _memoryOwner =
                     MemoryPool<byte>.Shared.Rent(65536);
@@ -224,7 +229,7 @@ Connection: Closed
                 internal void PortForwardResponseReceived(
                     byte[] buffer)
                 {
-                    PortForwardResponse =
+                    PortForwardResponse +=
                         Encoding.ASCII.GetString(buffer);
                     _responseReceived.Release();
                 }
@@ -248,20 +253,23 @@ Connection: Closed
                     if (WebSocketMessageReceived.Count >
                         Request.Length)
                     {
-                        // Set channel
-                        Memory.Span[0] =
-                            WebSocketMessageReceived[0];
-                        Encoding.ASCII.GetBytes(Response)
-                            .CopyTo(Memory.Slice(1));
-                        await webSocket.SendAsync(
-                                Memory.Slice(
-                                    0,
-                                    Response.Length +
-                                    1),
-                                WebSocketMessageType.Binary,
-                                true,
-                                cancellationToken)
-                            .ConfigureAwait(false);
+                        foreach (var response in FragmentedResponses)
+                        {
+                            // Set channel
+                            Memory.Span[0] =
+                                WebSocketMessageReceived[0];
+                            Encoding.ASCII.GetBytes(response)
+                                .CopyTo(Memory.Slice(1));
+                            await webSocket.SendAsync(
+                                    Memory.Slice(
+                                        0,
+                                        response.Length +
+                                        1),
+                                    WebSocketMessageType.Binary,
+                                    true,
+                                    cancellationToken)
+                                .ConfigureAwait(false);
+                        }
                         return true;
                     }
 
