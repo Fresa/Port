734a74dd8c91cf79aa5c6791b031109b8f2288e8
Fredrik Arvidsson <fredrik@fkan.se>
2020-07-13 19:23:18 +0200

    simplify rendering port forward settings

--- src/Client/HttpExtensions.cs
+++ src/Client/HttpExtensions.cs
@@ -1,13 +1,16 @@
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using Port.Shared;
 
 namespace Port.Client
 {
-    public static class HttpExtensions
+    internal static class HttpExtensions
     {
-        public static async Task<T> GetFromNewtonsoftJsonAsync<T>(
+        internal static async Task<T> GetFromNewtonsoftJsonAsync<T>(
             this HttpClient httpClient,
             string url,
             CancellationToken cancellationToken = default)
@@ -18,6 +21,23 @@ namespace Port.Client
             return JsonConvert.DeserializeObject<T>(
                 await message.Content.ReadAsStringAsync()
                     .ConfigureAwait(false));
+        }
+    }
+
+    internal static class PodExtensions
+    {
+        internal static IEnumerable<Pod> ThatHostsService(
+            this IEnumerable<Pod> pods,
+            Service service)
+        {
+            return pods.Where(
+                pod =>
+                    service.Selectors.Any(
+                        pair =>
+                            pod.Labels.Any(
+                                valuePair =>
+                                    valuePair.Key == pair.Key &&
+                                    valuePair.Value == pair.Value)));
         }
     }
 }
