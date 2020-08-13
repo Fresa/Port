345b8cbd6bf33cad6649e4829c455b9405b74cdf
Fredrik Arvidsson <fredrik@fkan.se>
2020-08-13 17:51:40 +0200

    write go away

--- src/Server/Spdy/GoAway.cs
+++ src/Server/Spdy/GoAway.cs
@@ -105,7 +105,15 @@ namespace Port.Server.Spdy
             IFrameWriter frameWriter,
             CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            await frameWriter.WriteUInt24Async(
+                    Length, cancellationToken)
+                .ConfigureAwait(false);
+            await frameWriter.WriteUInt32Async(
+                    LastGoodStreamId.Value, cancellationToken)
+                .ConfigureAwait(false);
+            await frameWriter.WriteUInt32Async(
+                    (uint)Status, cancellationToken)
+                .ConfigureAwait(false);
         }
     }
 }
