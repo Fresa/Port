5ab58ea941122e59624c7a9304a95ab1ff5c3ddf
Fredrik Arvidsson <fredrik@fkan.se>
2020-09-07 23:05:02 +0200

    catch any protocol parsing errors

--- src/Server/Spdy/Frames/Frame.cs
+++ src/Server/Spdy/Frames/Frame.cs
@@ -1,25 +1,38 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
+using Log.It;
+using Port.Server.Spdy.Primitives;
 
 namespace Port.Server.Spdy.Frames
 {
     public abstract class Frame
     {
+        private static readonly ILogger Logger = LogFactory.Create<Frame>();
+
         internal static async ValueTask<ReadResult<Frame>> TryReadAsync(
             IFrameReader frameReader,
             CancellationToken cancellation = default)
         {
-            var firstByte = await frameReader.PeekByteAsync(cancellation)
-                                             .ConfigureAwait(false);
-            var isControlFrame = (firstByte & 0x80) != 0;
-            if (isControlFrame)
+            try
             {
-                return await Control.TryReadAsync(frameReader, cancellation)
-                                    .ConfigureAwait(false);
-            }
+                var firstByte = await frameReader.PeekByteAsync(cancellation)
+                                                 .ConfigureAwait(false);
+                var isControlFrame = (firstByte & 0x80) != 0;
+                if (isControlFrame)
+                {
+                    return await Control.TryReadAsync(frameReader, cancellation)
+                                        .ConfigureAwait(false);
+                }
 
-            return await Data.TryReadAsync(frameReader, cancellation)
-                             .ConfigureAwait(false);
+                return await Data.TryReadAsync(frameReader, cancellation)
+                                 .ConfigureAwait(false);
+            }
+            catch (Exception e)
+            {
+                Logger.Error(e, "Error while parsing frame");
+                return ReadResult<Frame>.Error(RstStream.ProtocolError(UInt31.From(0)));
+            }
         }
 
         internal async ValueTask WriteAsync(
